6985c55976ad94c5157ad7f57093e3b6465f99ba
Maciej Rudnicki <maciejrudnicki82@gmail.com>
2017-12-02 22:05:56 +0100

    working od facebook api

--- src/album/views.py
+++ src/album/views.py
@@ -3,6 +3,7 @@
 from django.views.generic import View
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView, DeleteView
+from requests import request as req
 
 from django.shortcuts import render, redirect
 from django.views import generic
@@ -12,8 +13,7 @@ from django.core.urlresolvers import reverse_lazy
 from .models import Album, Photo, FilteredPhoto
 from .forms import PhotoForm, AlbumForm
 from .skimageCommands import SkimageController, FILTERS
-from Album_na_Zdjecia.forms import UserFormLogin
-
+from allauth.socialaccount.models import SocialAccount, SocialToken
 
 
 
@@ -25,7 +25,14 @@ class IndexView(SecuredUser, generic.ListView):
     context_object_name = 'album_list'
 
     def get(self, request, *args, **kwargs):
-        return super(IndexView, self).checkSession(self, request, *args, **kwargs)
+        super(IndexView, self).checkSession(self, request, *args, **kwargs)
+        res = SocialAccount.objects.get(provider='facebook', user_id=request.user.id)
+        if res:
+            user = ""
+            token = SocialToken.objects.get(account_id=SocialAccount.objects.get(user_id=request.user.id))
+
+            res = req('get', 'https://graph.facebook.com/v2.11/me?fields=id,name&access_token='+token.token)
+        return super(IndexView, self).get(self, request, *args, **kwargs)
 
     def get_queryset(self):
         return Album.objects.filter(user_id=self.request.user.id)
